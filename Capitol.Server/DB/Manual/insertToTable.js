69a41f3c041d2a593cf790cd0167d02c02710dd5
Jonathan91199 <jonathan.c911@gmail.com>
2021-05-05 22:28:21 +0300

    insertToTable - dealing with Boolean

--- Capitol.Server/DB/Manual/insertToTable.js
+++ Capitol.Server/DB/Manual/insertToTable.js
@@ -1,32 +1,34 @@
 let colors = require('colors')
-let mySql = require ('mysql')
+let mySql = require('mysql')
 let uuid = require('uuid')
 
 
-module.exports = function insertToTable(dataBase, tableName, columns, values){
-    let connection = mySql.createConnection({
-        host: dataBase.hostName,
-        user: dataBase.userName,
-        password: dataBase.password,
-        database: dataBase.dataBase
-    })
+module.exports = function insertToTable(dataBase, tableName, columns, values) {
+  let connection = mySql.createConnection({
+    host: dataBase.hostName,
+    user: dataBase.userName,
+    password: dataBase.password,
+    database: dataBase.dataBase
+  })
 
-    let allColumns = ''
-    let allValues = ''
-    columns.forEach(column => {
-      allColumns = allColumns.concat(`${column},`)
-    }) 
-    allColumns = allColumns.substring(0, allColumns.length -1)
-    
-    values.forEach(value => {
+  let allColumns = ''
+  let allValues = ''
+  columns.forEach(column => {
+    allColumns = allColumns.concat(`${column},`)
+  })
+  allColumns = allColumns.substring(0, allColumns.length - 1)
+
+  values.forEach(value => {
+    if (typeof(value)==="boolean") {
+      allValues = allValues.concat(`${value},`)
+    }
+    else
       allValues = allValues.concat(`'${value}',`)
-    }) 
-    allValues = allValues.substring(0, allValues.length -1)
-    
-
-    var sql = `INSERT INTO ${tableName} (${allColumns}) VALUES (${allValues})`;
-    connection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(colors.green.inverse("Inserted To Table !"))
-    });
+  })
+  allValues = allValues.substring(0, allValues.length - 1)
+  var sql = `INSERT INTO ${tableName} (${allColumns}) VALUES (${allValues})`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(colors.green.inverse("Inserted To Table !"))
+  });
 }
